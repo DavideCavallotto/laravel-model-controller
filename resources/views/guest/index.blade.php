0f6fd0a9ec8d239fcff9373ff05cf4212b3a7e2b
DavideCavallotto <davidecavallotto92@gmail.com>
2023-12-12 17:33:48 +0100

    add card

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -2,8 +2,28 @@
 
 @section('content')
     
-    @foreach ($movies as $movie)
-    <h2>{{$movie->title}}</h2>    
-    @endforeach
+<div class="container">
+    <div class="row">
+            @foreach ($movies as $movie)
+            <div class="col-3 py-3">                
+                <div class="card border-info-subtle" style="width: 18rem;">
+                    <div class="card-header bg-primary-subtle">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">{{$movie->original_title}}</h6>
+                    </div>
+                    <div class="card-body bg-body-tertiary">
+                      <p class="card-text">{{$movie->nationality}}</p>
+                      <p class="card-text">{{$movie->date}}</p>
+                      <p class="card-text">Vote: <span class="fw-bold">{{$movie->vote}}</span></p>
+                      
+                    </div>
+                  </div>
+                
+            </div>
+            @endforeach
+            
+        </div>
+    </div>
+
 
 @endsection
